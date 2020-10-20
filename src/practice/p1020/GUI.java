342d77d27bb84298765efdcbba656252db4f0041
Jiyoon Park <jiyoonpark0207@gmail.com>
2020-10-20 20:52:50 +0900

    gui for today

---
+++ src/practice/p1020/GUI.java
@@ -0,0 +1,21 @@
+package practice.p1020;
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI extends JFrame {
+    public GUI(){
+        setVisible(true);
+        setTitle("at least one a day");
+        setLayout( new GridLayout(2, 1));
+        setSize(300, 400);
+        JPanel j = new JPanel();
+        j.add(new JButton("I am a button"));
+        j.add(new JButton("I am a button"));
+        add(j);
+        add(j);
+        add(new JButton("Hello"));
+    }
+    public static void main(String [] args){
+        new GUI();
+    }
+}
