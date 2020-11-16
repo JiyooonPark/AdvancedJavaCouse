96b71672bb7593551755083a98cb443ae1beee42
Jiyoon Park <jiyoonpark0207@gmail.com>
2020-11-16 11:05:17 +0900

    lecture 10 - 1 updated

--- src/lecture10/KeyTest_color_change.java
+++ src/lecture10/KeyTest_color_change.java
@@ -2,8 +2,7 @@ package lecture10;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.*;
 
 public class KeyTest_color_change extends JFrame {
 
@@ -13,6 +12,9 @@ public class KeyTest_color_change extends JFrame {
     KeyTest_color_change(){
         setTitle("Key 이벤트처리");
         lbl = new JLabel("<Enter>키도 배경색이 변경됩니다");
+        //if want to use fonts again use
+        //Font f = new Font("", Font.BOLD, 20);
+
         lbl.setFont(new Font("", Font.BOLD, 20));
 
         c=getContentPane();
@@ -42,24 +44,52 @@ public class KeyTest_color_change extends JFrame {
         setVisible(true);
     }
     //1. define listener (inner class)
-    class MyKey implements KeyListener{
+//    class MyKey implements KeyListener{
+//
+//        @Override
+//        public void keyTyped(KeyEvent e) {
+//            //System.out.println(e.getKeyChar());
+//            System.out.println(e.getKeyCode());
+//            //why does this not work for me?
+//        }
+//
+//        @Override
+//        //the e in the param can be changed to anything
+//        public void keyPressed(KeyEvent e) {
+//           // System.out.println(e);
+//        }
+//
+//        @Override
+//        public void keyReleased(KeyEvent e) {
+//           // System.out.println(e);
+//
+//        }
+//    }
 
-        @Override
-        public void keyTyped(KeyEvent e) {
-            //System.out.println(e.getKeyChar());
-            System.out.println(e.getKeyCode());
-            //why does this not work for me?
+    class MyKey extends KeyAdapter implements ContainerListener {
+        public void keyPressed(KeyEvent e){
+            int r = (int) (Math.random()*256);
+            int g = (int) (Math.random()*256);
+            int b = (int) (Math.random()*256);
+            if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                String color;
+                c.setBackground((new Color(r,g,b)));
+                color = "( "+ r+" , "+g+" , "+b+" )";
+                lbl.setText(color);
+
+            }
+            if(e.getKeyChar()=='q'){
+                System.exit(0);
+            }
         }
 
         @Override
-        //the e in the param can be changed to anything
-        public void keyPressed(KeyEvent e) {
-           // System.out.println(e);
+        public void componentAdded(ContainerEvent e) {
+
         }
 
         @Override
-        public void keyReleased(KeyEvent e) {
-           // System.out.println(e);
+        public void componentRemoved(ContainerEvent e) {
 
         }
     }
